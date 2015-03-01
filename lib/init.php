3237514bbe1399194f11c384cb25be98566b238b
Luz M Costa <luzmcosta@gmail.com>
2015-03-01 11:57:43 -0500

    Adjusts widgets' misaligned code blocks.

--- lib/init.php
+++ lib/init.php
@@ -60,21 +60,21 @@ function roots_widgets_init() {
     ));
 
     register_sidebar(array(
-    'name'          => __('Twitter', 'roots'),
-    'id'            => 'sidebar-twitter',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
-  ));
+        'name'          => __('Twitter', 'roots'),
+        'id'            => 'sidebar-twitter',
+        'before_widget' => '<section class="widget %1$s %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ));
 
-  register_sidebar(array(
-    'name'          => __('Footer', 'roots'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
-  ));
+    register_sidebar(array(
+        'name'          => __('Footer', 'roots'),
+        'id'            => 'sidebar-footer',
+        'before_widget' => '<section class="widget %1$s %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ));
 }
 add_action('widgets_init', 'roots_widgets_init');
