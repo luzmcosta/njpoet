d588803c76b539af18cf3679dbf713671820b6aa
Luz M. Costa <luzmcosta@gmail.com>
2015-03-08 18:56:28 -0500

    Removes menu from header.

--- templates/header.php
+++ templates/header.php
@@ -14,17 +14,6 @@
             </div>
         </div>
 
-        <div class="header-navbar">
-            <nav class="collapse navbar-collapse" role="navigation">
-            <?php
-                if (has_nav_menu('primary_navigation')) {
-                    $walker = new Menu_With_Description;
-                    wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'walker' => $walker ) );
-                }
-            ?>
-            </nav>
-        </div>
-
         <div class="menu-icon">
             <a href="//twitter.com/charlesbivona">
                 <i class="fa fa-twitter fa-2x"></i>
