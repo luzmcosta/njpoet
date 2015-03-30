9889e76b1814dcdfd14610db4f2cf4d53b72e3fe
Luz M. Costa <luzmcosta@gmail.com>
2015-03-29 20:37:09 -0500

    Moves footer widget area below footer menu.

--- templates/footer.php
+++ templates/footer.php
@@ -16,16 +16,16 @@
         </ul>
     </div>
 
-    <div class="container">
-        <?php dynamic_sidebar('sidebar-footer'); ?>
-    </div>
-
     <div class="nav-menu">
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/contact">Contact</a></li>
         </ul>
+    </div>
+
+    <div class="container">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
 
     <div class="copyright">
